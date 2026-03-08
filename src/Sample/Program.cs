155cd4de3c7e452c1b9bb7b86c569d0ddb330f5d
Joel Verhagen <joel.verhagen@gmail.com>
2026-03-08 13:00:11 -0400

    Undo sandbox change

--- src/Sample/Program.cs
+++ src/Sample/Program.cs
@@ -5,59 +5,20 @@ CancellationToken token = CancellationToken.None;
 using FileStream zipStream = File.OpenRead(path);
 using SessionArchiveZip saz = SessionArchiveZip.Create(zipStream);
 
-foreach (Session session in saz.Sessions.Take(1))
+foreach (Session session in saz.Sessions.Take(3))
 {
-    for (var i = 0; i < 10; i++)
-    {
-        // read the request as an HTTP request message
-        using var request = await session.ReadRequestAsync(decompress: true, token);
-        Console.WriteLine($"Request: {request.Method} {request.RequestUri}");
+    // read the request as an HTTP request message
+    using var request = await session.ReadRequestAsync(decompress: true, token);
+    Console.WriteLine($"Request: {request.Method} {request.RequestUri}");
 
-        // read the response as an HTTP response message
-        using var response = await session.ReadResponseAsync(decompress: true, token);
-        var responseBody = await response.Content.ReadAsStringAsync(token);
-        Console.WriteLine($"Response: {(int)response.StatusCode} {response.ReasonPhrase}");
-        Console.WriteLine($"Body string length: {responseBody.Length}");
+    // read the response as an HTTP response message
+    using var response = await session.ReadResponseAsync(decompress: true, token);
+    var responseBody = await response.Content.ReadAsStringAsync(token);
+    Console.WriteLine($"Response: {(int)response.StatusCode} {response.ReasonPhrase}");
+    Console.WriteLine($"Body string length: {responseBody.Length}");
 
-        // read metadata about the request like timings
-        var metadata = await session.ReadMetadataAsync(token);
-        Console.WriteLine($"Started: {metadata.SessionTimers?.ClientBeginRequest:O}");
-        Console.WriteLine();
-    }
-
-    for (var i = 0; i < 10; i++)
-    {
-        // read the request as an HTTP request message
-        using var request = await session.ReadRequestAsync(decompress: true, token);
-        Console.WriteLine($"Request: {request.Method} {request.RequestUri}");
-
-        // read the response as an HTTP response message
-        using var response = await session.ReadResponseAsync(decompress: true, token);
-        var responseBody = await response.Content.ReadAsStringAsync(token);
-        Console.WriteLine($"Response: {(int)response.StatusCode} {response.ReasonPhrase}");
-        Console.WriteLine($"Body string length: {responseBody.Length}");
-
-        // read metadata about the request like timings
-        var metadata = await session.ReadMetadataAsync(token);
-        Console.WriteLine($"Started: {metadata.SessionTimers?.ClientBeginRequest:O}");
-        Console.WriteLine();
-    }
-
-    for (var i = 0; i < 10; i++)
-    {
-        // read the request as an HTTP request message
-        using var request = await session.ReadRequestAsync(decompress: true, token);
-        Console.WriteLine($"Request: {request.Method} {request.RequestUri}");
-
-        // read the response as an HTTP response message
-        using var response = await session.ReadResponseAsync(decompress: true, token);
-        var responseBody = await response.Content.ReadAsStringAsync(token);
-        Console.WriteLine($"Response: {(int)response.StatusCode} {response.ReasonPhrase}");
-        Console.WriteLine($"Body string length: {responseBody.Length}");
-
-        // read metadata about the request like timings
-        var metadata = await session.ReadMetadataAsync(token);
-        Console.WriteLine($"Started: {metadata.SessionTimers?.ClientBeginRequest:O}");
-        Console.WriteLine();
-    }
+    // read metadata about the request like timings
+    var metadata = await session.ReadMetadataAsync(token);
+    Console.WriteLine($"Started: {metadata.SessionTimers?.ClientBeginRequest:O}");
+    Console.WriteLine();
 }
